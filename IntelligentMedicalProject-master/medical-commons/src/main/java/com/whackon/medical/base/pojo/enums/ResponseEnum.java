d2a5cb1b6b3d19c11008dda7d992d97eb9e6e9cb
qck <942658285@qq.com>
2022-08-24 20:33:36 +0800

    提交第二次medical-commons

--- IntelligentMedicalProject-master/medical-commons/src/main/java/com/whackon/medical/base/pojo/enums/ResponseEnum.java
+++ IntelligentMedicalProject-master/medical-commons/src/main/java/com/whackon/medical/base/pojo/enums/ResponseEnum.java
@@ -1,4 +1,37 @@
 package com.whackon.medical.base.pojo.enums;
+/**
+ * <b>系统响应码枚举信息</b>
+ *
+ * @author qck
+ * @date 2022/8/24
+ */
+public enum ResponseEnum {
+	RESPONSE_SUCCESS(BaseEunmConstants.RESPONSE_SUCCESS,"系统业务处理和响应成功"),
+	RESPONSE_UNAUTH(BaseEunmConstants.RESPONSE_UNAUTH,"用户未进行系统认证"),
+	RESPONSE_FAILURE(BaseEunmConstants.RESPONSE_FAILURE,"系统业务处理错误"),
+	RESPONSE_EXCEPTION(BaseEunmConstants.RESPONSE_EXCEPTION,"系统响应异常");
 
-public class ResponseEnum {
+	private Integer code;                    // 系统响应编码
+	private  String remark;                  // 系统响应编码说明
+
+	private ResponseEnum(Integer code, String remark) {
+		this.code = code;
+		this.remark = remark;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
 }
--- IntelligentMedicalProject-master/medical-commons/src/main/java/com/whackon/medical/base/pojo/enums/StatusEnum.java
+++ IntelligentMedicalProject-master/medical-commons/src/main/java/com/whackon/medical/base/pojo/enums/StatusEnum.java
@@ -1,4 +1,35 @@
 package com.whackon.medical.base.pojo.enums;
+/**
+ * <b>系统状态枚举信息</b>
+ *
+ * @author qck
+ * @date 2022/8/24
+ */
+public enum StatusEnum {
+	STATUS_ENABLE(BaseEunmConstants.STATUS_ENABLE,"启用"),
+	STATUS_DISABLE(BaseEunmConstants.STATUS_DISABLE,"禁用");
 
-public class StatusEnum {
+	private String code;                        //状态编码
+	private String remark;                      //状态说明
+
+	private StatusEnum(String code, String remark) {
+		this.code = code;
+		this.remark = remark;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
 }
