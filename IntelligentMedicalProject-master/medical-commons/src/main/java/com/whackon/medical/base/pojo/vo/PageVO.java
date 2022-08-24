436b6c5a29e8f10acf2c0acd5c24e296a13d8546
Qt <cxiao1224@163.com>
2022-08-24 19:42:16 +0800

    创建了视图信息

--- IntelligentMedicalProject-master/medical-commons/src/main/java/com/whackon/medical/base/pojo/vo/PageVO.java
+++ IntelligentMedicalProject-master/medical-commons/src/main/java/com/whackon/medical/base/pojo/vo/PageVO.java
@@ -1,4 +1,43 @@
 package com.whackon.medical.base.pojo.vo;
 
-public class PageVO {
+import com.whackon.medical.base.util.BaseConstants;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * <b></b>
+ *
+ * @author Qt
+ * @date 2022/8/24
+ */
+@Data
+public class PageVO<E extends BaseVO> implements Serializable {
+	private static final long serialVersionUID = -2439429337168551684L;
+	private Integer pageNum;                    // 当前页码
+	private Integer pageSize;                   // 每页显示数量
+	private List<E> list;                       // 分页列表
+	private Long totalCount;                    // 总条数
+	private Integer totalPage;                  // 总页数
+
+	public PageVO(){}
+
+	public PageVO(Integer pageNum, Integer pageSize){
+		//判断所给定的当前页码是否有效
+		if (pageNum != null && pageNum > 0){
+			//所给定的当前页码有效
+			this.pageNum = pageNum;
+		}else {
+			//所给定的当前页码无效，则使用默认值
+			this.pageNum = BaseConstants.PAGE_NUM;
+		}
+
+		if(pageSize != null && pageSize > 0){
+			this.pageSize = pageSize;
+		}else {
+			this.pageSize = BaseConstants.PAGE_SIZE;
+		}
+	}
+
 }
+
--- IntelligentMedicalProject-master/medical-commons/src/main/java/com/whackon/medical/base/pojo/vo/ResponseVO.java
+++ IntelligentMedicalProject-master/medical-commons/src/main/java/com/whackon/medical/base/pojo/vo/ResponseVO.java
@@ -1,4 +1,68 @@
 package com.whackon.medical.base.pojo.vo;
 
-public class ResponseVO {
+import com.whackon.medical.base.pojo.enums.ResponseEnum;
+
+import java.io.Serializable;
+
+/**
+ * <b>系统响应视图信息</b>
+ * <p>
+ *     本项目所有和前端交互返回数据都必须以本响应试图信息对象返回。
+ * </p>
+ * @author Qt
+ * @date 2022/8/24
+ */
+@Data
+public class ResponseVO<E> implements Serializable {
+	private static final long serialVersionUID = 1466395221278356911L;
+	private Integer code;                         // 响应编码
+	private String message;                       // 响应信息
+	private E data;                               // 响应数据
+
+	public ResponseVO(ResponseEnum responseEnum, String message, E data){
+		this.code = responseEnum.getCode();
+		this.message = message;
+		this.data = data;
+	}
+
+	/**
+	 * <b>获得系统业务处理和响应成功视图信息</b>
+	 * @param message
+	 * @return
+	 */
+	public static ResponseVO ceateSuccessResponseVO(String message){
+		return new ResponseVO(ResponseEnum.RESPONSE_SUCCESS,message,new String(""));
+	}
+	/**
+	 * <b>获得系统业务处理和响应成功视图信息</b>
+	 * @param message
+	 * @param data
+	 * @return
+	 */
+	public static ResponseVO ceateSuccessResponseVO(String message, Object data){
+		return new ResponseVO(ResponseEnum.RESPONSE_SUCCESS,message,data);
+	}
+	/**
+	 * <b>获得用户未进行系统认证视图信息</b>
+	 * @return
+	 */
+	public static ResponseVO createUnAuthResponseVO(){
+		return new ResponseVO(ResponseEnum.RESPONSE_UNAUTH,ResponseEnum.RESPONSE_UNAUTH.getRemark(),new String(""));
+	}
+	/**
+	 * <b>获得系统业务处理错误视图信息</b>
+	 * @param errorMessage
+	 * @return
+	 */
+	public static ResponseVO createFailureResponseVO(String errorMessage){
+		return new ResponseVO(ResponseEnum.RESPONSE_FAILURE,errorMessage,new String(""));
+	}
+	/**
+	 * <b>获得系统响应异常视图信息</b>
+	 * @param exception
+	 * @return
+	 */
+	public static ResponseVO createExceptionResponseVO(Exception exception){
+		return new ResponseVO(ResponseEnum.RESPONSE_EXCEPTION,exception.getMessage(),exception);
+	}
 }
